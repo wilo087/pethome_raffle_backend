a8ef7000b5c372c1de96ec2be9933ffe0fd81948
Adrian Mendez <mendezadrian149@gmail.com>
2020-05-10 17:34:36 -0400

    adding query to list all user or specific and other query to get all user winner

---
+++ src/graphql/Raffle/Query.ts
@@ -0,0 +1,43 @@
+import { User, Context} from '../../utils'
+
+/**
+ * return all user
+ * or specific user
+ * @param parent 
+ * @param args 
+ * @param ctx 
+ */
+function getUser(parent: object, args: User, ctx: Context): User {
+    const {prisma} = ctx
+    const {id} = args
+
+    if(!id)
+        return prisma.users.findMany()
+   
+  return prisma.users.findOne({
+      where:{
+          id: Number(id)
+      }
+  })
+}
+
+/**
+ * return all
+ * users winner
+ * @param parent 
+ * @param args 
+ * @param ctx 
+ */
+function getAllWinner(parent: object, args: User, ctx: Context): User {
+    const {prisma} = ctx
+    return prisma.users.findMany({
+        where:{
+            winner: 1
+        }
+    })
+}
+
+
+export const Query = {
+    getUser, getAllWinner
+}
