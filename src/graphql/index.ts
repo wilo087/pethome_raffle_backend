0ca3e8a8a6d8e604031e9ea045cb3960e06938a1
Adrian Mendez <mendezadrian149@gmail.com>
2020-06-06 19:04:37 -0400

    adding config to extencion files in gql merge schema, .ts for development and .js for producction

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,10 +1,14 @@
 import { mergeResolvers, mergeTypes, fileLoader } from 'merge-graphql-schemas';
 import path from 'path';
 
+let extensionFiles: string = '.ts';
+
+if (process.env.NODE_ENV === 'producction') {
+    extensionFiles = '.js';
+}
+
 const typesArray: string[] = fileLoader(path.join(__dirname, '/**/*.graphql'));
-const resolversArray = fileLoader(path.join(__dirname, '**/**/resolver'),
-                                  { extensions: ['ts', 'js'] },
-);
+const resolversArray = fileLoader(path.join(__dirname, `**/resolver${extensionFiles}`));
 
 export const types: string = mergeTypes(typesArray);
 export const resolvers = mergeResolvers(resolversArray);
