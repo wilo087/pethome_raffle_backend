689f0a952aa83575c3d7e88e0a8e14f801590aee
Wilowayne De La Cruz G <wilo0087@gmail.com>
2020-06-26 00:22:11 -0400

    Adding express server with sample data

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.SERVER_PORT || 3000;
+const winner = {
+  id: '1',
+  name: 'Dayana Morente',
+  document: '22400119818',
+  code: '48859',
+}
+
+app.get('**', (req, res) => res.json(winner));
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
