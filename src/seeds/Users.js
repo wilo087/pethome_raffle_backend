9dbfb63f5f013ce24446bbf3b7d58c2bbe903b9e
Wilowayne De La Cruz G <wilo0087@gmail.com>
2020-06-28 01:42:10 -0400

    Adding user seeders

---
+++ src/seeds/Users.js
@@ -0,0 +1,29 @@
+const user = [
+  {
+    name: 'Dayana Morente',
+    document: '00120173201',
+    code: '48859'
+  },
+  {
+    name: 'Wilowayne De La Cruz',
+    document: '22400119818',
+    code: '020312'
+  },
+  {
+    name: 'Wilowayne De La Cruz',
+    document: '22400119818',
+    code: '99999'
+  },
+  {
+    name: 'Jan Guzman',
+    document: '24400129459',
+    code: '162010'
+  },
+  {
+    name: 'Adrian Mendez',
+    document: '00121448572',
+    code: '001520'
+  },
+]
+
+export default user;
