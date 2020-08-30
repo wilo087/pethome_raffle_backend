eac70ba49be0dc1ff73f72614057387ef16bc024
Wilowayne De La Cruz G <wilo0087@gmail.com>
2020-08-29 20:35:37 -0400

    Adding cors to request.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import cors from 'cors'
 import db from './models';
 
 const app = express();
-const port = process.env.SERVER_PORT || 3000;
+const port = process.env.SERVER_PORT || 3001;
 
+app.use(cors())
 app.get('/', (req, res) => res.json({ state: 'Success' }));
-app.get('/v1/winner', async (req, res) => {
+app.get('/api', async (req, res) => {
   const user = await db.User.findOne({
     where: { won: 0 },
     order: [
